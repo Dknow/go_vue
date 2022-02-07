a62161095328eaf42eace05c086c56cb7988b489
Dknow <dknow7758521@live.com>
2022-02-08 07:52:05 +0800

    add git file

---
+++ routers/router.go
@@ -0,0 +1,55 @@
+package routers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go_vue/routers/controllers"
+	//"database/sql"
+	//_ "github.com/mattn/go-sqlite3"
+)
+
+// db
+// mysql？ sqlite？
+// orm？
+const (
+	dbDriverName = "sqlite3"
+	dbName       = "./data.db3"
+)
+
+var count int32
+
+func ApiRoutersInit(r *gin.Engine) {
+	apiRouters := r.Group("/api")
+	{
+		apiRouters.GET("/count",countMiddleware, Count)
+		apiRouters.GET("/GetWebsiteList", countMiddleware,controllers.GetWebsiteList)
+	}
+}
+func AdminRoutersInit(r *gin.Engine) {
+	adminRouters := r.Group("/adminApi")
+	{
+		adminRouters.GET("/AddWebsite",countMiddleware, controllers.AddWebsite)
+		adminRouters.GET("/DeleteWebsite",countMiddleware, controllers.DeleteWebsite)
+		adminRouters.GET("/EditWebsite",countMiddleware, controllers.EditWebsite)
+	}
+}
+// get website list
+
+// admin
+
+// 分类 crud
+
+// website crud
+
+// count
+func countMiddleware(c *gin.Context){
+	count++
+	fmt.Println("count: ",count)
+}
+
+func Count(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"count":count ,
+	})
+}
+//	middleware
