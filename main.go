ad5f202158df5678e4493dcdce1935bbef6457a6
Dknow <dknow7758521@live.com>
2022-02-09 08:01:25 +0800

    orm

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go_vue/routers"
 	"gorm.io/driver/sqlite"
@@ -13,12 +14,19 @@ type Product struct {
 	Code  string
 	Price uint
 }
+//type Category struct {
+//	gorm.Model
+//	Id	int16
+//	Name string
+
+//}
 
 
 func main() {
 	//Default返回一个默认的路由引擎
 	r := gin.Default()
 	var count int
+	fmt.Println("count： ",count)
 
 	//db
 
@@ -29,11 +37,19 @@ func main() {
 
 	// Migrate the schema
 	db.AutoMigrate(&Product{})
-	db.Create(&Product{Title: "新款手机", Code: "D42", Price: 1000})
-	db.Create(&Product{Title: "新款电脑", Code: "D43", Price: 3500})
+	//db.Create(&Product{Title: "新款手机", Code: "D42", Price: 1000})
+	//db.Create(&Product{Title: "新款电脑", Code: "D43", Price: 3500})
 	//var product Product
-	//db.First(&product, 1) // find product with integer primary key
+	var products []Product
+
+	//db.First(&product) // find product with integer primary key
+	//fmt.Println(product.Title)
+
+	db.Find(&products)
+	fmt.Println(products.Ti)
+
 	//db.First(&product, "code = ?", "D42") // find product with code D42
+	//fmt.Println(product.Code)
 
 	r.GET("/", func(c *gin.Context) {
 		//输出json结果给调用方
